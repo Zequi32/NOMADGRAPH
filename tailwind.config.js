4672378a411f5ec712696f2699deea95edba1e8f
Ezequiel <david.ezequiel.quintero@gmail.com>
2024-07-20 09:12:04 -0500

    v8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./work.html",
-    "./contact.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Adjust paths if needed
+    './*.html',
+    './js/*.js',
   ],
   theme: {
     extend: {},
